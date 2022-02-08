b71c01e1b3b3f473fcb880bfcde9771c0742c83a
Uros Ivanovic <urosivanovich@gmail.com>
2022-02-07 19:45:11 -0500

    initial push

--- api/server.js
+++ api/server.js
@@ -1,3 +1,27 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.get('/api/users', (req, res) => {
+    User.find()
+    .then(users => {
+       res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting users',
+            err: err.message
+        })
+    })
+})
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+
+module.exports = server;
--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const server = require('./api/server');
 const port = 9000;
 
 // START YOUR SERVER HERE
+server.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
+
+console.log('hey you')
